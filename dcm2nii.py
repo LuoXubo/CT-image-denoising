8b11053f12610df5a4db97998f5a2d9feb38d3eb
Jeremy <53671855+LuoXubo@users.noreply.github.com>
2021-07-03 11:45:28 +0800

    将dicom序列转化成NIFTI文件

---
+++ dcm2nii.py
@@ -0,0 +1,18 @@
+import SimpleITK as sitk
+
+dcmpath = 'C:/Users/Administrator/Desktop/weiying'
+savepath = 'C:/Users/Administrator/Desktop/'
+
+reader = sitk.ImageSeriesReader()
+dicom_names = reader.GetGDCMSeriesFileNames(dcmpath)
+reader.SetFileNames(dicom_names)
+image2 = reader.Execute()
+image_array = sitk.GetArrayFromImage(image2)  # z, y, x
+origin = image2.GetOrigin()  # x, y, z
+spacing = image2.GetSpacing()  # x, y, z
+image3 = sitk.GetImageFromArray(image_array)  ##其他三维数据修改原本的数据，
+sitk.WriteImage(image3, savepath + 'weiying.nii')  # 这里可以直接换成image2 这样就保存了原来的数据成了nii格式了。
+
+
+
+
